1647358e2f05428ccf77a0f0d2bba913349bf755
Eli Parker <racecar47@icloud.com>
2024-10-25 12:16:03 -0600

    Updated projects with colorful buttons

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,6 +1,6 @@
 import { Key } from 'react';
 import projects from './Data/projects.json';
-import { Card } from 'flowbite-react';
+import { Button, Card } from 'flowbite-react';
 
 
 /**
@@ -31,7 +31,7 @@ function Projects()
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-2">
         
         {projects.projects.map((project: { name: string; description: string; siteReference: string; github: string; }, index: Key) => (
-            <Card key={index} href="#" className="m-6 max-w-sm">
+            <Card key={index} className="m-6 max-w-sm">
                 {/* Card title */}
                 <h5 className="text-2xl font-bold tracking-tight dark:text-slate-50">
                     {project.name}
@@ -47,27 +47,22 @@ function Projects()
 
                     {/* Site Reference (only display if there's a reference) */}
                     {project.siteReference != "" && (
-                        <a
-                            href={project.siteReference}
-                            className="
-                                inline-flex items-center rounded-lg bg-cyan-700 px-4 py-2 text-center text-sm font-medium
-                                text-slate-50 hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300
-                                dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
+                        <Button
+                            onMouseDown={(e: { button: number; }) => { if (e.button === 0 || e.button === 1) window.open(project.siteReference) }}
+                            outline gradientDuoTone="pinkToOrange"
                         >
                             Go to Project
-                        </a>
+                        </Button>
                     )}
-                    <a
+                    <Button
                         href={project.github}
                         className="
-                            inline-flex items-center rounded-lg border border-gray-300 bg-white
-                            px-4 py-2 text-center text-sm font-medium text-slate-50 hover:bg-gray-100
                             focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600
                           dark:bg-gray-800 dark:text-slate-50 dark:hover:border-gray-700 dark:hover:bg-gray-700
                           dark:focus:ring-gray-700"
                     >
                         See on GitHub
-                    </a>
+                    </Button>
                 </div>
             </Card>
         ))}
